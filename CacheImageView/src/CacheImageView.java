99d989477ec2b8f1aafefc39673b66ac0235e8f7
danil <danil@nm.ru>
2014-03-27 08:48:42 +0400

    создание основного класса

---
+++ CacheImageView/src/CacheImageView.java
@@ -0,0 +1,20 @@
+import android.content.Context;
+import android.util.AttributeSet;
+import android.widget.ImageView;
+
+
+public class CacheImageView extends ImageView {
+
+	public CacheImageView(Context context) {
+		super(context);
+	}
+
+	public CacheImageView(Context context, AttributeSet attrs) {
+		super(context, attrs);
+	}
+
+	public CacheImageView(Context context, AttributeSet attrs, int defStyle) {
+		super(context, attrs, defStyle);
+	}
+
+}
