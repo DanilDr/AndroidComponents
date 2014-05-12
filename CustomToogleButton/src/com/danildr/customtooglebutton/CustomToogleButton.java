265ab83c7591b3f249c6f1a843f8962fa3ecd4b2
DanilDr <danil@nm.ru>
2014-05-12 12:58:13 +0400

    Инициализация проекта кнопки ToogleButton

---
+++ CustomToogleButton/src/com/danildr/customtooglebutton/CustomToogleButton.java
@@ -0,0 +1,13 @@
+package com.danildr.customtooglebutton;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class CustomToogleButton extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.maintooglebutton);
+	}
+}
